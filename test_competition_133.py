242e18bd8c373e8caef418f2387338eda311e556
eoghanlochlyneoghanlochlyn <eoghanlochlyn@atomicmail.io>
2026-09-24 12:08:12 +0330

    Add exhaustive historical test for competition 133

---
+++ test_competition_133.py
@@ -0,0 +1,14 @@
+"""Exhaustive historical regression test for configured competition 133."""
+
+import unittest
+
+from historical_competition_test_helpers import run_exhaustive_competition_test
+
+
+class Competition133ExhaustiveTests(unittest.TestCase):
+    def test_every_match_of_latest_completed_season(self):
+        run_exhaustive_competition_test(self, "133")
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
